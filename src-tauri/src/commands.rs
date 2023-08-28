fbd6cc8927116b06cde7c0b59299ded6a987c564
sergioshtromb <81059697+sergashtromb@users.noreply.github.com>
2023-08-28 08:31:49 +0700

    Проверки

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -4,39 +4,55 @@ pub mod com {
     use serde_derive::{Deserialize, Serialize};
     use crate::db::db;
     use crate::tools::tasks;
+    use crate::tools::settings;
 
     #[tauri::command]
     pub async fn check_user_by_all(user_name: String, user_password: String) -> String {
 
-        let data_base = db::Db::connect().await.unwrap();
-        let new_user = data_base.get_user_by_all(user_name, user_password).await;
-        let result = serde_json::to_string(&new_user).unwrap();
+        if settings::GLOBAL_OPTIONS.lock().await.have_db {
+
+            let data_base = db::Db::connect().await.unwrap();
+            let new_user = data_base.get_user_by_all(user_name, user_password).await;
+            let result = serde_json::to_string(&new_user).unwrap();
         
-        return result;
+            return result;
+        }
+        
+        return "".to_string();
     }
 
     #[tauri::command]
     pub async fn check_user_by_name(user_name: String) -> String {
 
-        let data_base = db::Db::connect().await.unwrap();
-        let new_user = data_base.get_user_by_name(user_name).await;
-        let result = serde_json::to_string(&new_user).unwrap();
+        if settings::GLOBAL_OPTIONS.lock().await.have_db {
+
+            let data_base = db::Db::connect().await.unwrap();
+            let new_user = data_base.get_user_by_name(user_name).await;
+            let result = serde_json::to_string(&new_user).unwrap();
+            
+            return result;
+        }
         
-        return result;
+        return "".to_string();
     }
 
     #[tauri::command]
     pub async fn registration_user(user_name: String, user_password: String) -> String {
 
-        let data_base = db::Db::connect().await.unwrap();
-        let new_user = tasks::User {
-            id: data_base.create_user(user_name.clone(), user_password.clone()).await,
-            user_name: user_name,
-            user_password: user_password,
-        };
+        if settings::GLOBAL_OPTIONS.lock().await.have_db {
+            
+            let data_base = db::Db::connect().await.unwrap();
+            let new_user = tasks::User {
+                id: data_base.create_user(user_name.clone(), user_password.clone()).await,
+                user_name: user_name,
+                user_password: user_password,
+            };
 
-        let result = serde_json::to_string(&new_user).unwrap();
+            let result = serde_json::to_string(&new_user).unwrap();
 
-        return result;
+            return result;
+        }
+        
+        return "".to_string();
     }
 }
